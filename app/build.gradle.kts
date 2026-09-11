b1ac6255565ff9bddb159eefe4d94dab6da5092a
SHAMKIREC <konfeta.tvix@gmail.com>
2026-09-12 01:47:44 +0400

    chore(android): bump prototype version for HyperOS diagnostics build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "io.nrv.gameagent"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
