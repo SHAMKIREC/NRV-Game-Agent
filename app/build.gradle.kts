185bea62fa5177e78e9ab338d3f5c8c1f8753c01
SHAMKIREC <konfeta.tvix@gmail.com>
2026-09-13 03:09:57 +0400

    chore(version): bump WPC profile build to 0.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "io.nrv.gameagent.ai"
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.2.2"
+        versionCode = 6
+        versionName = "0.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
