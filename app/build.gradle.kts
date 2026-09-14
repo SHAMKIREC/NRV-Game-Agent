35d010343a371198a6b18668a0c0703a5d520641
SHAMKIREC <konfeta.tvix@gmail.com>
2026-09-14 17:02:17 +0400

    Bump version to 0.2.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "io.nrv.gameagent.ai"
         minSdk = 29
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.2.8"
+        versionCode = 12
+        versionName = "0.2.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
